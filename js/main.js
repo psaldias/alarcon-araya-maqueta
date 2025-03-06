d28c02ee9992525cc3dc80df18265df42f3ea00f
psaldias <psaldias@mandrildigital.cl>
2025-03-06 11:22:53 -0300

    Maqueta diseño fInalizada

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ const menu_button = document.querySelector("#menu-principal button");
 menu_button.addEventListener("click", () => {
   document.querySelector("#menu-principal .menu").classList.toggle("active");
 });
+
+const swiper = new Swiper(".swiper-clientes", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
